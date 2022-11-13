e2ddeb45e9920519a3168fde0feb58e06e20b384
PolinaBulgakova <peb.05062003@mail.ru>
2022-11-13 15:16:41 +0000

    Vector test

--- SpaceBattle.Lib/Vector.cs
+++ SpaceBattle.Lib/Vector.cs
@@ -35,13 +35,6 @@ public class Vector{
         }
     }
 
-    public static Vector operator * (int n, Vector v){
-        int[] sum = new int[v.size];
-            for (int i = 0; i < v.size; i++)
-                sum[i] = v[i] * n;
-            return new Vector(sum);
-    }
-
     public static bool operator == (Vector v1, Vector v2){
         if (v1.size != v2.size)
             return false;
@@ -55,38 +48,16 @@ public class Vector{
         return !(v1==v2);
     }
 
-    // public static bool operator < (Vector v1, Vector v2){
-    // if (v1.size != v2.size)
-    //     return v1.size < v2.size;
-
-    // if (v1 == v2)
-    //     return false;
-    // bool f = false;
-    // for (int i = 0; i < v1.size; i++){
-    //     if (v1[i] < v2[i])
-    //         f = true;
-    //     else if (v1[i] > v2[i])
-    //         return false;
-    // }
-    // return f;
-    // }
-    // public static bool operator > (Vector v1, Vector v2){
-    //     return !(v1<v2);
-    // }
-
-    // public override string ToString(){  
-    //     string s = "Vector(";
-    //     for (int i = 0; i < size - 1; i++)
-    //         s +=($"{vec[i]}, ");
-    //     s += ($"{vec[size - 1]})");
-    //     return s;
-    // }
-
-
     public override bool Equals(object? obj) => obj is Vector v && vec.SequenceEqual(v.vec);
-    
-    public override int GetHashCode() {return HashCode.Combine(vec);}
 
+    public override int GetHashCode() {
+        HashCode hash = new();
+        foreach(int i in vec){
+            hash.Add(vec[i]);
+        }
+        return hash.ToHashCode();
+
+    }
 }
 
 }
